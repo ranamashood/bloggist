9bfc0279c676188d53f3f5fa4c3e2cd18c78bdb1
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-03-10 16:47:36 +0500

    style: view blog styling

--- src/app/blog/view-blog/view-blog.component.ts
+++ src/app/blog/view-blog/view-blog.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Blog } from '../../blog.model';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { BlogsService } from '../../blogs.service';
 import { ActivatedRoute } from '@angular/router';
 import { AddCommentComponent } from '../../comment/add-comment/add-comment.component';
 import { ViewCommentsComponent } from '../../comment/view-comments/view-comments.component';
+import { BlogResponse } from '../../response.models';
 
 @Component({
   selector: 'app-view-blog',
@@ -13,7 +13,7 @@ import { ViewCommentsComponent } from '../../comment/view-comments/view-comments
   templateUrl: './view-blog.component.html',
 })
 export class ViewBlogComponent {
-  blog$: Observable<Blog> = new Observable();
+  blog$: Observable<BlogResponse> = new Observable();
   blogId = '';
 
   constructor(

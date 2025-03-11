5f54134ee053c25c77f02f37b9914fa449327c01
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-03-12 01:08:32 +0500

    feat: show avatar instead of just name in header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../../user.service';
 import { AsyncPipe } from '@angular/common';
+import { PreviewAvatarComponent } from '../../avatar/preview-avatar/preview-avatar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, AsyncPipe],
+  imports: [RouterLink, AsyncPipe, PreviewAvatarComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {

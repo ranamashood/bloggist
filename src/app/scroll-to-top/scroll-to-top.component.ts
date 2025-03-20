21eac109cac163f002e2b2a4b5f6935c19e50093
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-03-20 19:42:30 +0500

    feat: add back to top button

--- src/app/scroll-to-top/scroll-to-top.component.ts
+++ src/app/scroll-to-top/scroll-to-top.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-scroll-to-top',
-  imports: [NgIcon],
+  imports: [NgIcon, NgClass],
   templateUrl: './scroll-to-top.component.html',
   styleUrl: './scroll-to-top.component.css',
 })
 export class ScrollToTopComponent {
+  isVisible = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const screenHeight = window.screen.height;
+
+    this.isVisible =
+      document.body.scrollTop > screenHeight ||
+      document.documentElement.scrollTop > screenHeight;
+  }
+
   scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;

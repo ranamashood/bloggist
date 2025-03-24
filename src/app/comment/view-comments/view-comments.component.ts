068b7e4a6e275b70be7dbf1a126b249884fd8200
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-03-24 16:00:47 +0500

    refactor: use inject for comments

--- src/app/comment/view-comments/view-comments.component.ts
+++ src/app/comment/view-comments/view-comments.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommentsService } from '../../comments.service';
-import { Observable } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { CommonModule, NgFor } from '@angular/common';
 import { PreviewCommentComponent } from '../preview-comment/preview-comment.component';
-import { BlogResponse, CommentResponse } from '../../response.models';
+import { BlogResponse } from '../../response.models';
 
 @Component({
   selector: 'app-view-comments',
@@ -16,12 +15,9 @@ export class ViewCommentsComponent {
 
   @Input() blogId: string = '';
   @Input() blog: BlogResponse = {} as BlogResponse;
-  comments$: Observable<CommentResponse[]> = new Observable<
-    CommentResponse[]
-  >();
+  comments$ = inject(CommentsService).comments$;
 
   ngOnInit() {
     this.commentService.getComments(this.blogId);
-    this.comments$ = this.commentService.comments$;
   }
 }
